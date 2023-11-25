d9f6ad0caec8110127f6beccb5218dd561bacd85
Dibash Bkram Thapa <dwasthapa172.com>
2023-11-25 18:57:20 +0545

    AI consultancy change theme to white and dark blue.

--- components/hero.js
+++ components/hero.js
@@ -6,22 +6,22 @@ import Link from 'next/link';
 export default function hero() {
   return (
       <div className='flex flex-col md:relative'>
-        <div className='md:absolute bg-black md:bg-opacity-40  w-full lg:h-[43rem] md:h-[30rem] flex justify-center'>
-          <div className='w-[30rem] p-5 mt-5'>
+        <div className='md:absolute bg-black md:bg-opacity-40  w-full lg:h-[43rem] md:h-[30rem] flex flex-col justify-top items-center'>
+          <div className='w-[30rem] mt-5 pt-5'>
             <h2 className='text-white text-3xl'>One-stop shop for business setup and support services in Saudi Arabia and the UAE,
               powered by ChatGPT.
             </h2>
             <p className='pt-5 text-white'>
               Get started today
             </p>
-          </div>
-        </div>
-        <div className="md:absolute flex flex justify-end w-full md:h-[30rem] items-center place-content-center">
-          <Link href="/chat-al">
-            <Button className="md:transform md:rotate-[-90deg] p-5 hover:p-7 text-xl md:translate-x-10">Try ChatGPT-AL
+          <Link href="/ai-consultancy">
+            <Button className="hover:text-2xl text-xl ">AI Consultancy
             </Button>
           </Link>
+          </div>
         </div>
+        {/* <div className="md:absolute flex flex justify-end w-full md:h-[30rem] items-center place-content-center">
+        </div> */}
         <img className='order-first md:order-last w-full lg:h-[43rem] md:h-[30rem]' src={'/images/uae.webp'}/>
       </div>
   )
--- components/navbar.js
+++ components/navbar.js
@@ -65,7 +65,7 @@ const navListMenuServices = [
 const navListMenuBusiness = [
     {'FREE ZONES':
         [{
-            title: "OverviewE",
+            title: "Overview",
         },
         {
             title: "Business Activities",
